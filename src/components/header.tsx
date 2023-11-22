7f686267e6ef6d496a102c803f77a12ab5a66b46
João Victor Campos <ripjv1234@gmail.com>
2023-11-22 13:32:37 -0300

    exercicio-agora-é-pratica

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <h1>
+        Desenvolvimento Web
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/moduleTetaisl.tsx
@@ -0,0 +1,13 @@
+function ModuleDetaisl() {
+  return (
+    <ul style={ { textAlign: 'center' } }>
+      Módulos do Curso:
+      <li>✅Fundamentos</li>
+      <li>⏳Front-End</li>
+      <li>⏳Back-End</li>
+      <li>⏳Ciencia da Computação</li>
+      <li>⏳Soft-Skills</li>
+    </ul>
+  );
+}
+export default ModuleDetaisl;
